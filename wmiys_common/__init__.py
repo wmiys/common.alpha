f77132b5fa2274f2f8a7652411cd9246097cdc0a
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-11-02 18:54:03 -0500

    add constants

--- wmiys_common/__init__.py
+++ wmiys_common/__init__.py
@@ -1,2 +1,7 @@
-from . import constants as constants
+# utilities
 from . import utilities as utilities
+
+# constants
+from . import constants as constants
+from .constants import DevelopmentUrls as DevelopmentUrls
+from .constants import ProductionUrls as ProductionUrls
